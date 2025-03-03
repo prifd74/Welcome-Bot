754f796e2ebd25443bceafa68d7bfeac427e22a4
prifd74 <prifdgamer@gmail.com>
2025-03-03 15:19:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     token : "",
-    mongoURI : "",
+    mongoURI : "mongodb+srv://RTX:pokemon@cluster0.na7n4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
-    customStatus: "GlaceYT",
+    customStatus: "Chill",
     streamingURL: "https://www.twitch.tv/glaceytt"
 };
